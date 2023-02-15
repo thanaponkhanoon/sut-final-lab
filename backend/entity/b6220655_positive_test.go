a77ead844e25ce95ea47be1fcb9ef1133b2ebed4
thanaponkhanoon <thanaponkhanoon1123@gmail.com>
2023-02-15 10:41:06 -0800

    test cus -close #4

--- backend/entity/b6220655_positive_test.go
+++ backend/entity/b6220655_positive_test.go
@@ -1,1 +1,29 @@
 package entity
+
+import (
+	"testing"
+
+	"github.com/asaskevich/govalidator"
+	. "github.com/onsi/gomega"
+)
+
+func TestCustomerCus(t *testing.T) {
+	g := NewGomegaWithT(t)
+	cus := Customer{
+		Name:       "thanapon",
+		CustomerID: "",
+	}
+
+	// ตรวจสอบด้วย govalidator
+	ok, err := govalidator.ValidateStruct(cus)
+
+	// ok ต้องไม่เป็นค่า true แปลว่าต้องจับ error ได้
+	g.Expect(ok).ToNot(BeTrue())
+
+	// err ต้องไม่เป็นค่า nil แปลว่าต้องจับ error ได้
+	g.Expect(err).ToNot(BeNil())
+
+	// err.Error ต้องมี error message แสดงออกมา
+	g.Expect(err.Error()).To(Equal("รหัสลูกค้าไม่ถูกต้อง"))
+
+}
