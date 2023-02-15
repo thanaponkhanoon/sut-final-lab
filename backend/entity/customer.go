b82e9208b0dbdef2b11f579b9096bbd1f6cfff5a
thanaponkhanoon <thanaponkhanoon1123@gmail.com>
2023-02-15 10:34:35 -0800

    test all true -close #2

--- backend/entity/customer.go
+++ backend/entity/customer.go
@@ -8,5 +8,5 @@ type Customer struct {
 	gorm.Model
 	Name       string `valid:"required~กรุณากรอกชื่อ"`
 	Email      string
-	CustomerID string
+	CustomerID string `valid:"required~รหัสลูกค้าไม่ถูกต้อง ,matches(^([L|M|H])([0-9]{7}$))~รูปแบบเบอร์โทรผู้รับไม่ถูกต้อง"`
 }
